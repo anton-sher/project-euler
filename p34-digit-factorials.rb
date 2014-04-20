4d56c3f6e1e5cf4a304b9585a1320507a8a95cbd
Anton Sherstiuk <anton.sher@gmail.com>
2014-04-20 18:37:41 +0200

    Added solution 34

---
+++ p34-digit-factorials.rb
@@ -0,0 +1,21 @@
+require './project-euler-functions.rb'
+
+$fac = (0..9).map{|i| factorial(i)}.to_a
+
+numbers = []
+for n in (3..1000000) do
+	m = n
+	sd = 0
+	loop do
+		sd += $fac[m%10]
+		m /= 10
+		break if m == 0
+	end
+	if sd == n then
+		puts n
+		numbers << n
+	end
+end
+
+print numbers; puts
+printf "Answer is %d\n", numbers.reduce(:+)
