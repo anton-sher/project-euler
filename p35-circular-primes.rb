95155eee02332eb680ea2abe10adfb70117a0ff6
Anton Sherstiuk <anton.sher@gmail.com>
2014-04-20 21:51:01 +0200

    solution 35

---
+++ p35-circular-primes.rb
@@ -0,0 +1,33 @@
+require './project-euler-functions.rb'
+
+def circulate n
+	s = n.to_s
+	l = s.length
+	res = []
+	l.times{
+		res << s.to_i
+		s = s[1..-1] + s[0]
+
+	}
+	return res
+end
+
+circular_numbers = []
+
+for p in (2..1000000) do
+	c = circulate p
+	match = true
+	for a in c do
+		if !is_prime(a) then
+			match = false
+			break
+		end
+	end
+	circular_numbers << p if match
+	if p % 100000 == 0 then
+		puts "processed " + p.to_s
+	end
+end
+
+print circular_numbers; puts
+printf "The answer is %d\n", circular_numbers.length 
