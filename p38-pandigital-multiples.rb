7f2e1429b838446f5e588151f57d29a7cc490dcc
Anton Sherstiuk <anton.sher@gmail.com>
2014-04-22 22:05:07 +0200

    solution to problem 38

---
+++ p38-pandigital-multiples.rb
@@ -0,0 +1,17 @@
+require './p32-pandigital.rb'
+require './project-euler-functions.rb'
+
+pandigitals = []
+for n in 1..9999 do
+	m = 0
+	a = 1
+	while number_of_digits(m) < 9 do
+		q = n * a
+		m = m * (10 ** number_of_digits(q)) + q
+		a += 1
+	end
+	pandigitals << m if is_pandigital_to(m, 9)
+end
+
+print pandigitals, "\n"
+printf "The answer is %d\n", pandigitals.max
